dfbb8dac3e5fc1074d82ebb55b71c18a767e5bb2
JustAlternate <lololollololol890@gmail.com>
2023-08-30 20:08:49 +0200

    hypr config

--- neofetch/change_distro.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-distro=$(shuf -n 1 list_all_distro_name | head -1)
-last_distro=$(cat last_one)
-echo $distro
-echo $distro > last_one
-sed -i 's/'$last_distro'/'$distro'/g' config.conf
